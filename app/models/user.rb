5ec6666e48077ac34596210372abc34ef2089ff8
Christie Hua <christiehua@gmail.com>
2014-07-27 01:13:04 -0700

    active serializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,25 @@
+require "bcrypt"
 class User < ActiveRecord::Base
+	has_many :resources
+  
+  #field :is_admin, type: Boolean, default: false
+  validates_presence_of :name, :email, :password_digest
+ 
+
+  def password
+    @password
+  end
+
+  def password=(new_password)
+    @password = new_password
+    self.password_digest = BCrypt::Password.create(new_password)
+  end
+
+  def authenticate(test_password)
+    if BCrypt::Password.new(self.password_digest) == test_password
+      self
+    else
+      false
+    end
+  end
 end
